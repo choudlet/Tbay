256d8a94149e51352fbecc54c1cad607dc5bb5fc
choudlette <choudlette@gmail.com>
2015-06-09 22:54:06 -0400

    all models created

--- tbay.py
+++ tbay.py
@@ -2,9 +2,9 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from datetime import datetime
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, Numeric
 
-engine = create_engine('postgresql://action:action@localhost:5432/tbay')
+engine = create_engine('postgresql://christopherhoudlette:action@localhost:5432/tbay')
 Session = sessionmaker(bind=engine)
 session = Session()
 Base = declarative_base()
@@ -16,3 +16,18 @@ class Item(Base):
 	name = Column(String, nullable=False)
 	description = Column(String)
 	start_time = Column(DateTime, default=datetime.utcnow)
+	
+class User(Base):
+	__tablename__ = "users"
+	
+	id = Column(Integer, primary_key=True)
+	username = Column(String, nullable=False)
+	password = Column(String, nullable=False)
+	
+class Bid(Base):
+	__tablename__ = "bids"
+	id = Column(Integer, primary_key=True)
+	price = Column(Numeric, nullable=False)
+
+	
+Base.metadata.create_all(engine)
